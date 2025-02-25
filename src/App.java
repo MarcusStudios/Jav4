752a960ffaa81d7f55181fbbc0693f7e46a38c21
MarcusStudios <marcuseduardo846@gmail.com>
2025-02-25 15:53:52 -0400

    calculadora

--- src/App.java
+++ src/App.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double num1;
+        double num2;
+        double result;
+
+        String operator;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        num1 = input.nextDouble();
+
+        System.out.print("Enter the operator: ");
+        operator = input.next();
+
+        System.out.print("Enter the second number: ");
+        num2 = input.nextDouble();
+
+        switch (operator) { 
+            case "+":
+                result = num1 + num2;
+                System.out.println(num1 + " + " + num2 + " = " + result);
+                break;
+            case "-":
+                result = num1 - num2;
+                System.out.println(num1 + " - " + num2 + " = " + result);
+                break; 
+            case "*":
+                result = num1 * num2;    
+                System.out.println(num1 + " * " + num2 + " = " + result);
+                break;
+            case "/":
+                result = num1 / num2;
+                System.out.println(num1 + " / " + num2 + " = " + result);
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                break;
+        }
+        input.close();
     }
 }
